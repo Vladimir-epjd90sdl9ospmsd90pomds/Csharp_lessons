52ac1f6b773410e3d6fa4d8b146ec985abe19950
Vladimir-epjd90sdl9ospmsd90pomds <vololodja@yandex.ru>
2023-05-21 16:39:54 +0500

    First task is ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Семинар. Задание 1.
+Console.WriteLine("Введите целое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадрат вашего числа: " + num * num);
+
