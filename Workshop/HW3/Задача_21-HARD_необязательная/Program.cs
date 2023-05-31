16b291332ae20b8e89c70bd4de8fac4a9f79be0c
Vladimir-epjd90sdl9ospmsd90pomds <vololodja@yandex.ru>
2023-06-01 00:23:46 +0500

    21-st HARD is ready

--- Workshop/HW3/Задача_21-HARD_необязательная/Program.cs
+++ Workshop/HW3/Задача_21-HARD_необязательная/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними
+//  в N-мерном пространстве. Сначала задается N с клавиатуры, потом задаются координаты точек.
+
+
+Console.Clear();
+Console.WriteLine("Введите кол-во измерений");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arrayA = new int[num];
+int[] arrayB = new int[num];
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+
+    Console.WriteLine($"Введите координату {i + 1}-ого измерения точки А");
+    arrayA[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+
+for (int i = 0; i < arrayA.Length; i++)
+{
+
+    Console.WriteLine($"Введите координату {i + 1}-ого измерения точки B");
+    arrayB[i] = Convert.ToInt32(Console.ReadLine());
+
+}
+
+void lengthCalculate(int[] arrayA, int[] arrayB)
+{
+    double length = arrayA[0] - arrayB[0];
+    for (int i = 1; i < arrayA.Length; i++)
+    {
+
+        length = Math.Sqrt(Math.Pow(length, 2) + Math.Pow(arrayA[i] - arrayB[i], 2));
+    }
+
+    Console.WriteLine($"Длина отрезка: {length}");
+}
+
+lengthCalculate(arrayA, arrayB);
