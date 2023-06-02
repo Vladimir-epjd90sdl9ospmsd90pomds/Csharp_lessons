2ade3deb0ad0f0bb61b054043759e29c034f4f92
Vladimir-epjd90sdl9ospmsd90pomds <vololodja@yandex.ru>
2023-06-02 12:28:36 +0500

    realy ready hw4

--- Workshop/HW4/Задача_29/Program.cs
+++ Workshop/HW4/Задача_29/Program.cs
@@ -2,11 +2,16 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] array01 = new int[8];
-int ran = 0;
-for (int i = 0; i < array01.Length; i++)
+void randomArray()
 {
-    ran = new Random().Next(0, 100);
-    array01[i] = ran;
-    Console.Write($"{array01[i]} ");
+    int[] array01 = new int[8];
+    int ran = 0;
+    for (int i = 0; i < array01.Length; i++)
+    {
+        ran = new Random().Next(0, 100);
+        array01[i] = ran;
+        Console.Write($"{array01[i]} ");
+    }
 }
+
+randomArray();
