fd90f222fb31d7cd2f56fb555b91a0767f323709
Vladimir-epjd90sdl9ospmsd90pomds <vololodja@yandex.ru>
2023-07-29 17:03:55 +0500

    LinkedList

---
+++ Workshop/Algorithms course/Linked list/Program.cs
@@ -0,0 +1,31 @@
+﻿
+public class LinkedList
+{
+    Node head;
+    public class Node
+    {
+        int value;
+        Node next;
+    }
+    public void revert()
+    {
+        if (head != null && head.next != null)
+        {
+            revert(head.next, head);
+        }
+    }
+
+    private void revert(Node currentNode, Node previousNode)
+    {
+        if (currentNode.next == null)
+        {
+            head = currentNode;
+        }
+        else
+        {
+            revert(currentNode.next, currentNode);
+        }
+        currentNode.next = previousNode;
+        previousNode.next = null;
+    }
+}
